19400ba919f68bbf78ea93e8c69cb0d20c841dda
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-27 14:06:02 -0500

    Fixed Temporary Database Integration issue

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer, DateTime, ForeignKey)
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, sessionmaker
 
 # Choose from list of options  - user input
 
@@ -10,9 +10,13 @@ from sqlalchemy.orm import relationship, backref
 Base = declarative_base()
 Engine = create_engine("sqlite:///project.db")
 
+
+
 class Traveler(Base):
     
     __tablename__ = 'Travelers'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+
     id = Column(Integer(), primary_key = True)
     first_name = Column(String())
     last_name = Column(String())
@@ -21,6 +25,7 @@ class Traveler(Base):
 class Domicile(Base):
     
     __tablename__ = 'Domiciles'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
 
     id = Column(Integer(), primary_key = True)
     dest_location = Column(String())
@@ -37,14 +42,17 @@ class Domicile(Base):
 class Vacation(Base):
     
     __tablename__ = 'Vacations'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+
     id = Column(Integer(), primary_key = True)
     start_date = Column(DateTime())
     end_date = Column(DateTime())
-    Traveler_id = Column(Integer(),ForeignKey('Travelers.id'))
-    Domicile_id = Column(Integer(),ForeignKey('Domiciles.id'))
+    Traveler_id = Column(Integer(),ForeignKey('Travelers.id', name = "t-id_constraint"))
+    Domicile_id = Column(Integer(),ForeignKey('Domiciles.id', name = "d-id constraint"))
 
     # traveler = relationship('Travelers', backref=backref("traveler"))
     # lodging = relationship('Domicile', backref=backref("lodging"))
 
+# Base.metadata.create_all(bind=Engine)
 
 
