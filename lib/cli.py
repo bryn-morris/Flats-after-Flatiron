a7d6ac236bf55337431b97b38d7a4cd2e55296dc
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-28 15:50:45 -0400

    modifying start function

--- lib/cli.py
+++ lib/cli.py
@@ -10,7 +10,7 @@ class CLI():
     engine = create_engine("sqlite:///lib/db/project.db")
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     domiciles = session.query(Domicile).all()
     # what does this return?
 
@@ -23,7 +23,7 @@ class CLI():
         self.first_name = user_fn
         self.last_name = user_ln
         self.city = user_city
-        print('hi')
+        self.start()
 
     @property
     def traveler(self):
@@ -36,18 +36,31 @@ class CLI():
         session.commit()
         return new_traveler
     
-    def start(self):
-        pass
-    
-    def showDomiciles(self):
-            print(f"Property Type:{d.property_type}, Location: {d.dest_location}" for d in CLI.domiciles)
+    def start(self, book, view, browse):
+        print('')
+        print('WELCOME TO FLATS AFTER FLATIRON!')
+        print('')
 
-    def showData(self):
+        exit = False
+
+        while exit == False:
+            choice = input("What would you like to do? Type 'B' to book a vacation, Type 'V' to view your Vacations, or Type 'N' to browse ")
+            print('')
+
+            if choice.lower() == 'b':
+                pass
+            elif choice.lower() == 'v':
+                pass
+            elif choice.lower() == 'n':
+                browse()
+
+
+    def browse(self):
         user_action = input("Type D to see a list of Domiciles, V to see a list of vacations booked: ")
         print(' ')
 
         if user_action == 'D' or user_action == 'd':
-            showDomiciles()
+            print(f"Property Type:{d.property_type}, Location: {d.dest_location}" for d in CLI.domiciles)
         elif user_action == 'V' or user_action == 'v':
             pass
 
