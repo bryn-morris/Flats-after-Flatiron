67020467bf0a4d5a4251ba7a74f29a45a935285c
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-03-28 17:02:36 -0400

    added browse functionality for cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-
+import ipdb
 from db.models import Vacation, Traveler, Domicile
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("sqlite:///lib/db/project.db")
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class CLI():
-    engine = create_engine("sqlite:///lib/db/project.db")
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+    
     domiciles = session.query(Domicile).all()
     # what does this return?
 
@@ -36,7 +36,7 @@ class CLI():
         session.commit()
         return new_traveler
     
-    def start(self, book, view, browse):
+    def start(self):
         print('')
         print('WELCOME TO FLATS AFTER FLATIRON!')
         print('')
@@ -44,7 +44,7 @@ class CLI():
         exit = False
 
         while exit == False:
-            choice = input("What would you like to do? Type 'B' to book a vacation, Type 'V' to view your Vacations, or Type 'N' to browse ")
+            choice = input("What would you like to do? Type 'B' to book a vacation, Type 'V' to view/update your Vacations, or Type 'N' to browse all properties: ")
             print('')
 
             if choice.lower() == 'b':
@@ -52,18 +52,39 @@ class CLI():
             elif choice.lower() == 'v':
                 pass
             elif choice.lower() == 'n':
-                browse()
+                self.browse()
 
-
+            print(' ')
+            user_input = input("Would you like to stop now? (Type Y/N): ")
+            print(' ')
+            if user_input == "Y" or user_input == 'y':
+                exit = True
+          
     def browse(self):
-        user_action = input("Type D to see a list of Domiciles, V to see a list of vacations booked: ")
-        print(' ')
+        print('')
+        print('** Properties **')
+        print('')
 
-        if user_action == 'D' or user_action == 'd':
-            print(f"Property Type:{d.property_type}, Location: {d.dest_location}" for d in CLI.domiciles)
-        elif user_action == 'V' or user_action == 'v':
-            pass
+        for i, d in enumerate(CLI.domiciles):
+            print(f'{i + 1}. Property Type: {d.property_type}, Location: {d.dest_location}')
 
+        detailPropID = input('If you would like to see the details of a property please enter the number of the property in the list: ')
+
+        if int(detailPropID) in range(1, len(CLI.domiciles) + 1):
+            dp = CLI.domiciles[int(detailPropID) - 1]
+            print('** Property Details **')
+            print(f"Property Type: {dp.property_type}")
+            print(f"Location: {dp.dest_location}")
+            print(f"Sleeping Capacity: {dp.sleep_capacity}")
+            print(f"Local Amenities: {dp.local_amenities}")
+
+            viewPastBookings = input('Would you like to see the past bookings of this property? (y/n):')
+
+            if viewPastBookings == 'Y' or viewPastBookings == 'y':
+                pastVacations = [v for v in CLI.vacations if v.Domicile_id  == dp.id]
+                for v in pastVacations:
+                    print(v)
+            
 
 
 if __name__ == '__main__':
@@ -72,3 +93,6 @@ if __name__ == '__main__':
     user_ln = input("Enter Your Last Name: ")
     user_city = input("Enter Your City Name: ")
     CLI(user_fn, user_ln, user_city)
+
+
+ipdb.set_trace()
